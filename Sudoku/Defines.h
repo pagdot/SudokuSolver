9795c31069d416d40f255d1227991bc4f2801d75
Simon Danglmaier <S1610306004@students.fh-hagenberg.at>
2018-06-11 15:05:14 +0200

    Add 2 files
    Defines file for type definitions adn FileInput for FileOperations

---
+++ Sudoku/Defines.h
@@ -0,0 +1,26 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+//System Includes
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <array>
+#include <list>
+
+//Varibale declarations
+static const size_t base = 3;
+
+
+//Type declarations
+typedef std::array<std::array<size_t, base*base>, base*base> TSudoku;
+typedef std::list<size_t> TSudokuField;
+typedef std::array<std::array<TSudokuField, base*base>, base*base> TSudokuFields;
+
+
+
+
+
+
+#endif
---
+++ Sudoku/FileInput.h
@@ -0,0 +1,16 @@
+#ifndef FILEINPUT_H
+#define FILEINPUT_H
+
+#include <string>
+
+#include "Defines.h"
+
+bool VerifyInputFile(std::string const & file);
+
+TSudoku ReadFile (size_t const & index = 0);
+
+
+
+
+
+#endif
